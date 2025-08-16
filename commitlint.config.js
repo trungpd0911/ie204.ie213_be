b18026598597d7f700b7c6dfbff49a5b454dc65c
Phạm Thanh Duy <124453882+ptduy2603@users.noreply.github.com>
2025-08-16 20:57:08 +0700

    feat: update refresh token generation

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,6 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-        'type-enum': [2, 'always', ['feature', 'fixbug', 'setup', 'release']],
-        'type-case': [2, 'always', 'lower-case']
-    },
-}
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-case': [2, 'always', 'lower-case'],
+  },
+};
--- src/auth/dto/refreshRequest.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
-
-export class RefreshRequest {
-	@ApiProperty({ example: 'your refresh token' })
-	@IsNotEmpty()
-	@IsString()
-	refreshToken: string;
-}
